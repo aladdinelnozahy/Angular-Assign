725014c476b67fc3a3efffc46d0d406bf6fec713
aladdinelnozahy <aladdin.elnozahy@gmail.com>
2022-07-14 17:30:47 +0200

    testing view child day 3

--- src/app/Components/order-master/order-master.component.ts
+++ src/app/Components/order-master/order-master.component.ts
@@ -1,18 +1,32 @@
+import { ContentComponent } from './../content/content.component';
 import { ICategory } from './../../Models/icategory';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-order-master',
   templateUrl: './order-master.component.html',
   styleUrls: ['./order-master.component.scss']
 })
-export class OrderMasterComponent implements OnInit {
+export class OrderMasterComponent implements OnInit, AfterViewInit {
 
   categoryList: ICategory[];
   SelectedCatID:number= 0;
 
   // property to recieve total price
   recievedOrderTotalPrice:number=0;
+
+  // save navigation operator (?)> means clientName is optional may has val or not
+  //--------! >> non null assertion operation >> don't have
+  // @ViewChild('clientName') clientName?:ElementRef;
+
+  @ViewChild('clientName') clientName!:ElementRef;
+  // Refrance to component
+  @ViewChild(ContentComponent) contentComponentRef!:ContentComponent
+  // --------------------
+  // @ViewChild('clientName') clientName:ElementRef={} as ElementRef;
+  //------------- in this two define must use condition---------------
+  // @ViewChild('clientName') clientName:ElementRef|null=null;
+  // @ViewChild('clientName') clientName:ElementRef|undefined=undefined;
 
   constructor() {
     this.categoryList=
@@ -24,10 +38,19 @@ export class OrderMasterComponent implements OnInit {
       {ID: 5, Name: 'baby'}
     ]
   }
+  ngAfterViewInit(): void {
+    // if(this.clientName)
+    this.clientName.nativeElement.value="Value added from TS"
+    // console.log(this.contentComponentRef.prdListOfCat)
+  }
 
   ngOnInit(): void {
   }
   onTotalPriceChanged(totalPrice:number){
     this.recievedOrderTotalPrice=totalPrice;
+  }
+  getNewArrOfProd(){
+    console.log(this.contentComponentRef.prdListOfCat)
+
   }
 }
